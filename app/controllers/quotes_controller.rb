a5367ca105cf3ab71ab60cc19561bbcf9bff04ca
Will Daskam <willd2122@gmail.com>
2019-06-15 05:13:33 +0000

    added form modal

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,10 +2,6 @@ class QuotesController < ApplicationController
   def index
     @quote = Quote.order("Random()").first
   end
-  
-  def new
-    @quote = Quote.new
-  end
   def create
     @quote = Quote.create(quote_params)
     if @quote.invalid?
